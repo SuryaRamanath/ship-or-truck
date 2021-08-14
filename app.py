4a1d13d313877be66976d100984f4549775db159
SuryaRamanath <vrsurya80@gmail.com>
2021-08-14 21:40:55 +0530

    app.py changed

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 from PIL import Image
 from tensorflow.keras.preprocessing.image import load_img
@@ -17,7 +16,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 
 
 shape_ = (32,32,3)
-out = {9:"Truck",8:"Ship"}
+out = {1:"Truck",0:"Ship"}
 load_model = tf.keras.models.load_model("shiptruck_model")
 
 app = Flask(__name__)
@@ -30,20 +29,28 @@ def main():
 def predict():
     print("hello")
     if request.method == 'POST':
-        
+        # img_send = request.files['file']
         img_requested = request.files['file'].read()
         img = Image.open(io.BytesIO(img_requested))
+        
+        print(type(img))
         if img.mode != 'RGB':
             img = img.convert('RGB')
         img = img.resize((32, 32))
+        # img = img_to_array(img)
         img = np.array(img)
         img = np.expand_dims(img, axis=0)
         img = img.astype('float32')
         img = img / 255.0
-
+        # image = Image.open(io.BytesIO(img_send))
+        #img = img.resize((32,32))
+        # img = np.expand_dims(img,axis=1)
+        # img = preprocess_input(img, mode='caffe')
+        # img = np.array(img)
+       
+       
         pred = load_model.predict(img)
-        # print(pred)
-        # print(out.get(pred,default=None))
+        print(np.argmax(pred))
     return render_template("index.html", prediction = out[np.argmax(pred)]) #, img_path = img_path
 if __name__ == '__main__':
     app.run(port=8080,debug=True)
